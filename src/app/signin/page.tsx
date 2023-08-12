e9fef5f1f529f131ce0d0b7542ec26a9c9afc937
bunmigrey <bunmigrey@icloud.com>
2023-08-12 06:30:10 +0100

    web and mobile view signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,67 @@
-
+import Image from "next/image";
+import Header from "../components/globals/Header";
+import "./signin.scss";
+import emailIcon from "../../../public/emailIcon.svg";
+import passwordIcon from "../../../public/passwordIcon.svg";
 
 
 export default function Signin() {
 
 
     return(
-        <div></div>
+        <div>
+            <div className="mainDiv">
+                <Header></Header>
+                <div className="subMainDiv">
+                    <div className="subMainDiv1">
+                    </div>
+                    <div className="subMainDiv2">
+                        <div className="subMainDiv2Form">
+                            <p className="signupDiv">SIGN IN</p>
+                            <form>
+                                <div className="inputDiv">
+                                    <label htmlFor="email">EMAIL</label>
+                                    <Image
+                                        src={emailIcon}
+                                        alt="email icon"
+                                        width={20}
+                                        className="emailIcon"
+                                        height={15}
+                                    ></Image>
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </div>
+
+                                <div className="inputDiv">
+                                    <label htmlFor="password">PASSWORD</label>
+                                    <Image
+                                        src={passwordIcon}
+                                        alt="password icon"
+                                        width={20}
+                                        className="passwordIcon"
+                                        height={15}
+                                    ></Image>
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                </div>
+                                <button className="submitButton" type="submit">SIGN IN</button>
+                                <div className="alternateSignin">
+                                    <p>I don't have an account,</p>
+                                    <p>SIGN UP</p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
