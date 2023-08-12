33a24b0bd07234d58fcac25d02f37a54078e2b91
bunmigrey <bunmigrey@icloud.com>
2023-08-12 02:21:17 +0100

    signuo page 60% done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,9 @@
-import gazelogo from "../../../public/svgs/globals/gazelogo.svg";
 import Image from "next/image";
 import Header from "../components/globals/Header";
 import "./signup.scss";
+import usernameIcon from "../../../public/usernameIcon.svg";
+import emailIcon from "../../../public/emailIcon.svg";
+import passwordIcon from "../../../public/passwordIcon.svg";
 
 export default function Signup() {
 
@@ -11,7 +13,65 @@ export default function Signup() {
             <div className="mainDiv">
                 <Header></Header>
                 <div className="subMainDiv">
-                    <p>lets do it</p>
+                    <div className="subMainDiv1">
+                    </div>
+                    <div className="subMainDiv2">
+                        <div className="subMainDiv2Form">
+                            <p>SIGN UP</p>
+                            <form>
+                                <div className="inputDiv">
+                                    <label htmlFor="username">USERNAME</label>
+                                    <Image
+                                        src={usernameIcon}
+                                        alt="username icon"
+                                        width={20}
+                                        className="usernameIcon"
+                                        height={15}
+                                    ></Image>
+                                    <input
+                                        id="username"
+                                        name="username"
+                                        placeholder="Username"
+                                        required
+                                    />
+                                </div>
+
+                                <div className="inputDiv">
+                                    <label htmlFor="email">EMAIL</label>
+                                    <Image
+                                        src={emailIcon}
+                                        alt="email icon"
+                                        width={20}
+                                        className="emailIcon"
+                                        height={15}
+                                    ></Image>
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </div>
+
+                                <div className="inputDiv">
+                                    <label htmlFor="password">PASSWORD</label>
+                                    <Image
+                                        src={passwordIcon}
+                                        alt="password icon"
+                                        width={20}
+                                        className="passwordIcon"
+                                        height={15}
+                                    ></Image>
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
