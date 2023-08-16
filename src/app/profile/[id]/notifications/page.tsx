4f86096093ec67405b1af0b1d7da8f6bf6f9ecd0
Derin Owoade <owoadederin6@gmail.com>
2023-08-16 01:54:26 +0100

    Minor change

--- src/app/profile/[id]/notifications/page.tsx
+++ src/app/profile/[id]/notifications/page.tsx
@@ -3,7 +3,6 @@ export default function Notifications() {
   return (
     <div className="w-full px-[10px]">
       <div className="mt-[37px] flex xl:px-[30px] 2xl:px-[14px] flex-col gap-y-[20px] pb-[40px] sm:pb-[50px]">
-        {" "}
         {[1, 2, 3, 4, 6, 7, 8].map((item, index) => {
           return (
             <NotificationItem
