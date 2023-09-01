5e3ac42693d2ad5aacc8a7ae293c71bd4290ec02
Derin Owoade <owoadederin6@gmail.com>
2023-09-01 17:51:37 +0100

    Changes to Profile page

--- src/app/components/profile/ProfileHeader.tsx
+++ src/app/components/profile/ProfileHeader.tsx
@@ -1,8 +1,25 @@
+"use client";
 import profileHeaderAvatar from "../../../../public/svgs/profile/profile-header-avatar.svg";
 import Image from "next/image";
-
+import { UserPageContext } from "../UserPageContext";
+import { useContext } from "react";
+import { UserPageContextTypes } from "../UserPageContext";
 
 export default function ProfileHeader() {
+  const {
+    isSubscribed,
+    isValidated,
+    askPermissionAndUpdate,
+    getNftListing,
+    address,
+    unsubscribe,
+    totalNft,
+    nftCollectionListing,
+    collectionName,
+    collectionContractAddress,
+    setCollectionContractAddress,
+    username,
+  } = useContext(UserPageContext) as UserPageContextTypes;
   return (
     <div className="w-full h-full  xl:max-h-[60px] max-h-[45px] backdrop-blur-sm border-b border-black fixed top-0 right-0 fixed-cont flex items-center">
       <div className="w-full h-full relative flex items-center">
@@ -16,7 +33,8 @@ export default function ProfileHeader() {
           ></Image>
         </div>
         <div className="absolute right-[24px] lg:right-[36px] font-raleWay font-bold lg:text-[0.8rem] text-[0.8rem]">
-          <span className="text-neonGreen">Welcome</span>, KOKOMASTER2431.
+          <span className="text-neonGreen">Welcome</span>,{" "}
+          {username ? username : "user"}.
         </div>
       </div>
     </div>
