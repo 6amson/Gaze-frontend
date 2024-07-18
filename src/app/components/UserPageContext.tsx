2d29eb76c077bb5362044a4ca0cd34fa05584058
Derin Owoade <owoadederin6@gmail.com>
2024-07-18 08:26:47 +0100

    replaced former koyeb api url with current koyeb api url

--- src/app/components/UserPageContext.tsx
+++ src/app/components/UserPageContext.tsx
@@ -61,7 +61,7 @@ export default function UserPageProvider(props: UserPageProviderProps) {
 
   const vapidControl = process.env.NEXT_PUBLIC_VAPIDPUBLICKEYS;
   /*   const url = "http://[::1]:4000/"; */
-  const url = "https://previous-doralia-gaze.koyeb.app/";
+  const url = process.env.NEXT_PUBLIC_API_PATH;
 
   const [isSubscribed, setIsSubscribed] = useState(false);
   const [metamaskAddr, setMetamaskAddr] = useState(null);
